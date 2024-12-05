6166ac4d2848ced9d2f44248eb8dece777ea26b1
Phong Do <dominhphong306@gmail.com>
2024-12-05 21:45:43 +0700

    modify test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,12 +7,6 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('get started link', async ({ }) => {
+  expect(1).toBe(2);
 });
